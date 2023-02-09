9cd5febe8911d08186a8e8ac71d9d5232d095fba
João Victor <jvfernandes.contato@gmail.com>
2023-02-09 08:49:25 -0300

    feat: implementing order rendering on frontEnd boards

--- frontEnd/fe/src/components/Orders/index.tsx
+++ frontEnd/fe/src/components/Orders/index.tsx
@@ -1,36 +1,19 @@
+import { useEffect, useState } from 'react';
 import { Order } from '../../types/Order';
+import { api } from '../../utils/api';
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './styles';
 
-const orders: Order[] = [
-  {
-    '_id': '63cafc78575089323eb22e04',
-    'table': '123',
-    'status': 'WAITING',
-    'products': [
-      {
-        'product': {
-          'name': 'Pizza quatro queijos',
-          'imagePath': '1674245010478-quatro-queijos.png',
-          'price': 40,
-        },
-        'quantity': 3,
-        '_id': '63cafc78575089323eb22e05'
-      },
-      {
-        'product': {
-          'name': 'Coca cola',
-          'imagePath': '1674246191131-coca-cola.png',
-          'price': 7,
-        },
-        'quantity': 2,
-        '_id': '63cafc78575089323eb22e06'
-      }
-    ],
-  }
-];
-
 export function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    api.get('/orders')
+      .then(({ data }) => {
+        setOrders(data);
+      });
+  }, []);
+
   return (
     <Container>
       <OrdersBoard
@@ -42,13 +25,13 @@ export function Orders() {
       <OrdersBoard
         icon="🧑‍🍳️"
         title="Em preparação"
-        orders={[]}
+        orders={orders}
       />
 
       <OrdersBoard
         icon="✅️"
         title="Pronto!"
-        orders={[]}
+        orders={orders}
       />
     </Container>
   );
